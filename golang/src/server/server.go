ea1bb74ebb27d3644cadbb5a35ed1afd28388d0d
JamshedVesuna <jamshed.vesuna@gmail.com>
2015-08-26 10:07:59 -0700

    Cleaned up server; added usage

--- golang/src/server/server.go
+++ golang/src/server/server.go
@@ -1,5 +1,14 @@
-// main ...
-// TODO(jvesuna): Add usage with example input flags.
+// Package server implements a simple UDP server that ACKs every valid packet it receives.
+//
+// Example usage from pingtest/bin/
+//  # Runs a basic server *locally* that returns each valid UDP packet received.
+//	./server
+//
+//	# Assert ports to receive and send on and to.
+//	./server --client_ip=192.168.2.3 --client_rcv_port=1234 --server_rcv_port=2000 --server_snd_port=2001
+//
+//	# ACKs will not contain the padding and thus be consistently small.:w
+//	./server --tiny_ack=true
 package main
 
 import (
@@ -17,30 +26,23 @@ import (
 
 var (
 	clientIP      = flag.String("client_ip", "127.0.0.1", "The IP address of the sending client.")
-	serverIP      = flag.String("server_ip", "127.0.0.1", "The IP address of the ack server.")
 	clientRcvPort = flag.Int("client_rcv_port", 3141, "The client's port that listens for ACKs")
-	clientSndPort = flag.Int("client_snd_port", 3142, "The client's port that sends large packets.")
 	serverRcvPort = flag.Int("server_rcv_port", 3143, "The server's port that listens for packets.")
-	serverSndPort = flag.Int("server_snd_port", 3144, "The server's port that sends ACKS.")
-	randomize     = flag.Bool("randomize", true, "Randomize the UDP packet padding.")
+	serverSndPort = flag.Int("server_snd_port", 3144, "The server's port that sends ACKs.")
+	tinyAck       = flag.Bool("tiny_ack", false, "The server sends small, fixed sized ACKs instead of the entire payload.")
 )
 
-// checkError just logs the error.
-//TODO(jvesuna): Remove this function with better error handling.
-func checkError(err error) {
-	if err != nil {
-		log.Printf("Error: ", err)
-	}
-}
+const (
+	// maxUDPSize is the maximum size, in bytes, of a single UDP packet.
+	maxUDPSize = 65000
+)
 
 // invalidParams checks the input parameters and returns an error.
 func invalidParams() error {
-	// Must run as EITHER client or server.
 	// All ports must be unique.
 	ports := make(map[int]int)
-	for _, port := range []int{*clientRcvPort, *clientSndPort, *serverRcvPort, *serverSndPort} {
+	for _, port := range []int{*clientRcvPort, *serverRcvPort, *serverSndPort} {
 		if _, portExists := ports[port]; portExists {
-			// TODO(jvesuna): Confirm that all ports must be unique.
 			return errors.New("All ports must be unique")
 		}
 		ports[port] = 1
@@ -48,81 +50,80 @@ func invalidParams() error {
 	return nil
 }
 
-/////////////////////////////// SERVER ///////////////////////////////
-
-// ackServer simply sends a small ACK packet back to the sender for each packet is receives.
-//TODO(jvesuna): Send TCP packets.
+// ackServer simply sends an ACK packet back to the sender for each valid UDP packet is receives.
+// TODO(jvesuna): Send TCP packets instead of UDP for reliability.
 func ackServer() {
-
-	var serverWG sync.WaitGroup
-
+	// Bind to local port.
 	ServerAddr, err := net.ResolveUDPAddr("udp", ":"+strconv.Itoa(*serverRcvPort))
-	checkError(err)
-
+	if err != nil {
+		log.Fatalln("error binding to local port:", err)
+	}
 	ServerConn, err := net.ListenUDP("udp", ServerAddr)
-	checkError(err)
+	if err != nil {
+		log.Fatalln("error listening for UDP:", err)
+	}
 	defer ServerConn.Close()
 
+	// Wait for all threads to complete before exiting.
+	var serverWG sync.WaitGroup
 	for {
-		// TODO(jvesuna): This buff should be > 65,000 bytes.
-		buf := make([]byte, 65000)
+		buf := make([]byte, maxUDPSize)
 		numBytesRead, _, err := ServerConn.ReadFromUDP(buf)
-		log.Println("Got packet")
 		if err != nil {
-			log.Println("Error: ", err)
+			log.Println("error receiving packet: ", err)
 		}
-		// ACKS do not need to be ordered, so we do not need to lock.
-		// TODO(jvesuna): Ensure goroutine closes.
 		serverWG.Add(1)
-		// TODO(jvesuna): Make this a goroutine.
-		parseAndAck(buf[:numBytesRead])
+		go parseAndAck(buf[:numBytesRead])
 	}
 	serverWG.Wait()
 }
 
-// parseAndAck parses the payload and sends a tiny ACK to the Client Receiver.
-// TODO(jvesuna): Fix input to be a packet struct.
+// parseAndAck parses the UDP payload and sends anwACK to the client receiver.
 // TODO(jvesuna): Keep parseAndAck routine live with a connection. Have the ackServer channel data
 // to this routine for faster processing.
-// TODO(jvesuna): Fix error handling.
 func parseAndAck(buf []byte) {
-	message := &ptpb.PingtestMessage{}
-	if err := proto.Unmarshal(buf, message); err != nil {
+	messageAck := &ptpb.PingtestMessage{}
+	if err := proto.Unmarshal(buf, messageAck); err != nil {
 		//TODO(jvesuna): Fix this error handling.
-		log.Println("FIX: Failed to unmarshal in parseAndAck:", err)
+		log.Println("error: failed to unmarshal packet in parseAndAck:", err)
 	}
 
-	// TODO(jvesuna): Uncomment the following 3 blocks to send tiny ACKS.
-	//log.Println("Received ", message)
-	//paramsReceived := message.PingtestParams
+	if *tinyAck {
+		// Don't include padding.
+		// Here, messageAck should always be about the same size.
+		messageAck = &ptpb.PingtestMessage{
+			PingtestParams: messageAck.PingtestParams,
+			Type:           messageAck.Type,
+		}
+	}
 
-	//messageAck := &ptpb.PingtestMessage{
-	//PingtestParams: paramsReceived,
-	//}
+	wireBytesAck, err := proto.Marshal(messageAck)
+	if err != nil {
+		fmt.Println("error marshalling ACK:", err)
+	}
 
-	// wireBytesAck should always be about the same size. For now, we are testing sending the full
-	// payload back.
-	//wireBytesAck, err := proto.Marshal(messageAck)
-	wireBytesAck, err := proto.Marshal(message)
-	checkError(err)
-
-	// Prepare to send ACK
-	serverSenderAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:"+strconv.Itoa(*clientRcvPort))
-	checkError(err)
+	// Bind all addresses and ports.
+	// TODO(jvesuna): Fix addressing, use flags.
+	serverSenderAddr, err := net.ResolveUDPAddr("udp", *clientIP+":"+strconv.Itoa(*clientRcvPort))
+	if err != nil {
+		log.Fatalln("error binding to client address and port:", err)
+	}
 	// TODO(jvesuna): Fix port binding.
-	LocalAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:"+strconv.Itoa(*serverSndPort))
-	checkError(err)
+	LocalAddr, err := net.ResolveUDPAddr("udp", ":"+strconv.Itoa(*serverSndPort))
+	if err != nil {
+		log.Fatalln("error binding to local port:", err)
+	}
 	Conn, err := net.DialUDP("udp", LocalAddr, serverSenderAddr)
-	checkError(err)
+	if err != nil {
+		log.Fatalln("error making connection to client:", err)
+	}
 	defer Conn.Close()
 
 	// Send ACK.
-
-	Conn.SetWriteBuffer(proto.Size(message))
+	Conn.SetWriteBuffer(proto.Size(messageAck))
 	_, err = Conn.Write(wireBytesAck)
 	if err != nil {
-		//TODO(jvesuna): Fix.
-		log.Fatal("Failed to send ACK:", err)
+		log.Println("Failed to send ACK:", err)
 	}
 	log.Println("Sent Ack")
 }
@@ -130,10 +131,8 @@ func parseAndAck(buf []byte) {
 func main() {
 	flag.Parse()
 	if err := invalidParams(); err != nil {
-		log.Println("Error:", err)
-		return
+		log.Fatalln("Error:", err)
 	}
-	// ACK server simply ACKS each packet it receives.
-	fmt.Println("Running as server")
+	fmt.Println("Running UDP ACK server")
 	ackServer()
 }
